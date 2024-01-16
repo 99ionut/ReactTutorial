cabfd7e3f5a18721de340cc7ecd664847082eb96
Ionut Suciu <suciuionut888@gmail.com>
2024-01-16 15:08:01 +0100

    simple nesting example

--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //questo file è un js entry point
 
+//CSS
+import "./index.css";
+
 //la funzione deve avere la prima lettera grande cosi react sa che è un componente
 //ritorno html (jsx)
 
-function Greetings(){
-    return <h1>ciao</h1>
+function Booklist(){
+    return (
+      <div>
+        <Book/>
+        <Title/>
+      </div>
+      
+    )
+}
+const Title = () =>{
+  return <section style={{color:"blue"}}>
+    title
+  </section>
+}
+
+const Book = () =>{
+  return <section style={{color:"red", fontWeight:"bold", fontSize:"30px"}}>
+    book
+  </section>
 }
 
 //cerca cosa c'è da renderizzare e dove
-ReactDOM.render(<Greetings/>, document.getElementById("root"));
+ReactDOM.render(<Booklist/>, document.getElementById("root"));
 
