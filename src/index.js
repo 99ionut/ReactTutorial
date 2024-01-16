ae03f46a81228af65ee0a57e8159a4f9c383528d
Ionut Suciu <suciuionut888@gmail.com>
2024-01-16 15:52:37 +0100

    come passare un parametro unico ad un solo componente usando children

--- src/index.js
+++ src/index.js
@@ -13,21 +13,25 @@ let author = "BOOK AUTHOR";
 function Booklist(){
     return (
       <div>
-        <Book title="ok1 we" author="gggg"/>
-        <Book title="oeqwe f" author="ppp"/>
-        <Book title="o 2323 2" author="twrwr"/>
+        <Book title="ok1 we" author="gggg">
+          <p>Paragrafo solo primo elemento</p>
+        </Book>
+        <Book title="oeqwe f" author="ppp"></Book>
+        <Book title="o 2323 2" author="twrwr"></Book>
       </div>
       
     )
 }
 
 
-const Book = ({title, author}) =>{
+const Book = ({title, author, children}) =>{
   return (
     <section style={{color:"red", fontWeight:"bold"}}>
       <h1>{title}</h1>
       <h4>{author}</h4>
       <p>{6 + 6}</p>
+      {children}
+      <p>--------------</p>
     </section>
   )
 }
