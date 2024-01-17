ff1dca842b9592d4b529d43e132acf52780106cc
Ionut Suciu <suciuionut888@gmail.com>
2024-01-17 09:14:54 +0100

    click event with handler

--- src/index.js
+++ src/index.js
@@ -43,12 +43,17 @@ function Booklist(){
 
 
 const Book = (props) =>{
+  //event handler
+  const clickHandler = () =>{
+    alert("hello");
+  }
   const {title, author} = props;
   return (
     <section style={{color:"red", fontWeight:"bold"}}>
       <h1>{title}</h1>
       <h4>{author}</h4>
       <p>{6 + 6}</p>
+      <button type='button' onClick={clickHandler}>PRESS</button>
       <p>--------------</p>
     </section>
   )
