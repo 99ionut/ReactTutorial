46a60036dbdb7de98548e37e473d84bac15d765e
Ionut Suciu <suciuionut888@gmail.com>
2024-01-18 11:10:50 +0100

    new advanced project files commit

--- src/index.js
+++ src/index.js
@@ -1,36 +1,11 @@
 import React from 'react';
-//serve a injectare il JS nell'html
-import ReactDOM from 'react-dom';//questo file è un js entry point
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-//import books func from books file
-import {books} from './books.js'
-//this import can be named whatever
-import DifferentBookName from './book.js'
-//CSS
-import "./index.css";
-//la funzione deve avere la prima lettera grande cosi react sa che è un componente
-
-//vars
-let author = "BOOK AUTHOR";
-
-
-
-function Booklist(){
-    return (
-      <div>
-        {books.map((book, index)=>{
-          return(
-            <DifferentBookName key={book.id} {...book} />
-          );
-        })}
-      </div>
-      
-    )
-}
-
-
-
-
-//cerca cosa c'è da renderizzare e dove
-ReactDOM.render(<Booklist/>, document.getElementById("root"));
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
