9bc2fdd8f3c54de0d9473fca3eea0f1b97ea1026
Ionut Suciu <suciuionut888@gmail.com>
2024-01-18 15:30:08 +0100

    changing state value re renders

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text,setText] = useState("random Title")
+  const handleClick = () => {
+    if(text=="random Title"){
+      setText("hello world");
+    }else{
+      setText("random Title");
+    }
+    
+  }
+  return (
+    <>
+      <h2>{text}</h2>
+      <button onClick={handleClick}>CLICK</button>
+  </>
+  );
 };
 
 export default UseStateBasics;
