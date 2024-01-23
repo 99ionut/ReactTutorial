4d7cd91d71c621db8f8ad7863c1a08fb9a2ebe92
Ionut Suciu <suciuionut888@gmail.com>
2024-01-23 12:05:59 +0100

    async function doesnt get new state but the current one, clickig 3 times doesnt work

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -3,6 +3,13 @@ import React, { useState } from 'react';
 const UseStateCounter = () => {
   const [value,setValue] = useState(0);
 
+  /*clicking 3 times doesnt work*/
+  const complexIncrease = () => {
+    setTimeout(()=>{
+      setValue(value+1);
+    },2000)
+  }
+
   return( 
   <>
   <section>
@@ -11,6 +18,12 @@ const UseStateCounter = () => {
     <button onClick={() => setValue(value-1)}>decrease</button>
     <button onClick={() => setValue(value+1)}>increase</button>
     <button onClick={() => setValue(0)}>reset</button>
+  </section>
+
+  <section>
+    <h2>complex counter</h2>
+    <h1>{value}</h1>
+    <button onClick={complexIncrease}>increase later</button>
   </section>
   </>
   );
