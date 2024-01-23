a7a9d072dab10cca7c0a718de5032e966db92f10
Ionut Suciu <suciuionut888@gmail.com>
2024-01-23 15:17:09 +0100

    useEffect async await and no infinite loop

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,35 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users,setUsers] = useState([]);
+
+  const getUsers = async() =>{
+    const response = await fetch(url);
+    const users = await response.json();
+    console.log(users);
+    setUsers(users);
+  }
+
+  //you cant use async await here, thats why we use a diff function
+  useEffect(()=>{
+    getUsers();
+    //if u dont add this, infinite loop because the setUsers in the function you call triggers rerender.
+  }, [])
+
+  return (
+    <>
+    <ul>
+    <h1>users</h1>
+    {users.map((user)=>{
+      return (
+      <li>{user.login}
+      <img src={user.avatar_url} style={{"width":"40px"}}/>
+      </li>
+      )
+    })}
+    </ul>
+    </>
+  )
 };
 
 export default UseEffectFetchData;
