a416e9eb524733dd16224a499a8084c780eb20de
Ionut Suciu <suciuionut888@gmail.com>
2024-01-23 14:49:47 +0100

    useEffect important removeEVentListener and when to use it

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -5,19 +5,27 @@ import React, { useState, useEffect } from 'react';
 
 const UseEffectCleanup = () => {
   const [size,setSize] = useState(window.innerWidth);
-  console.log(size);
 
   const checkSize = () =>{
     setSize(window.innerWidth)
   }
 
   useEffect(()=>{
-    console.log("hello world");
+    console.log("use effect");
     //if you look at console in the event listeners tab you will see 10s of window resize listeners,
     //this will caouse a memory leak
-     window.addEventListener("resize",checkSize)
+    window.addEventListener("resize",checkSize);
+    //this will be called when you exit
+    return ()=>{
+      console.log("cleanup----");
+      window.removeEventListener("resize",checkSize)
+    }
+
+    //or you could add [] so it applies the event listener once at the initial render, but if you have a component
+    //that appears and disappears, you need to rerun it every time and clean after.
   })
 
+  console.log("render");
   return (
   <>
   <h1>Window</h1>
