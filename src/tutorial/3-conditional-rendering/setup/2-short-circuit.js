bdfeaa6b6761323485868e68495ac26bbcfe51c4
Ionut Suciu <suciuionut888@gmail.com>
2024-01-24 16:00:25 +0100

    the short-circuit || and && operator for returns, and when to use them

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,28 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text,setText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+  <>
+    <h1>first: {firstValue}</h1>
+    <h1>second: {secondValue}</h1>
+  {
+  //this doesnt work, we need an expression that returns a value
+  //if(){console.log("hello world")}
+  }
+    {
+      //this ALWASY return something, either text or something else
+    }
+    <h1>{text || "default text"}</h1>
+    {
+      //this returns ONLY return if text is true
+    }
+    <h1>{text && <h1>Hello world</h1>}</h1>
+  </>
+  );
 };
 
 export default ShortCircuit;
