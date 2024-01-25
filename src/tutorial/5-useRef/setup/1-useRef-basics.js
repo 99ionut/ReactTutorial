685637c17388a31baa68bdf57ab094980d065f41
Ionut Suciu <suciuionut888@gmail.com>
2024-01-25 11:50:30 +0100

    useRef works like useState but does not trigger render, used to target dom elements

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -4,8 +4,34 @@ import React, { useEffect, useRef } from 'react';
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
+//works like useState but does not trigger render, used to taget dom elements
+//to manage uncontrolled inputs
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(refContainer.current.value);
+    console.log(divContainer.current);
+  }
+
+  useEffect(() => {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  })
+
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input type='text' ref={refContainer}/>
+        <button type='submit'>Submit</button>
+      </div>
+    </form>
+    <div ref={divContainer}>Hello world</div>
+    </>
+  )
 };
 
 export default UseRefBasics;
