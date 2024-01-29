4cb765dd337dc00c7bfad66538c76909cba27899
Ionut Suciu <suciuionut888@gmail.com>
2024-01-29 15:57:32 +0100

    simple prop drilling example, not functional if alot of components

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -8,6 +8,7 @@ import {data} from "../../../data";
 const PropDrilling = () => {
   const [people,setPeople] = useState(data);
 
+  //how do you pass this all the way to single person?
   const removePerson = (id) => {
     setPeople((people)=>{
       return people.filter((person)=>person.id !== id);
@@ -17,27 +18,28 @@ const PropDrilling = () => {
   return (
     <>
       <h3>propr drilling</h3>
-      <List people={people}/>
+      <List people={people} removePerson={removePerson}/>
     </>
   );
 };
 
-const List = ({people})=>{
+const List = ({people,removePerson})=>{
   return (
   <>
     {
      people.map((person)=>{
-      return <SinglePerson key={person.id}/>
+      return <SinglePerson key={person.id} {...person} removePerson={removePerson}/>
      }) 
     }
   </>
   )
 }
 
-const SinglePerson = ({id,item}) => {
+const SinglePerson = ({id,name,removePerson}) => {
   return (
   <div>
-    <h4>single item</h4>
+    <h4>{name}</h4>
+    <button onClick={()=>removePerson(id)}>remove</button>
   </div>
   )
 }
