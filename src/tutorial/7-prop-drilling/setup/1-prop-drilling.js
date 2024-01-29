cdc88249cd469f9d84b5600f2ef57ec011891f5e
Ionut Suciu <suciuionut888@gmail.com>
2024-01-29 15:51:31 +0100

    list setup to show prop drilling example

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,45 @@
 import React, { useState } from 'react';
-
+import {data} from "../../../data";
 // more components
 // fix - context api, redux (for more complex cases)
 
+
+//when you have to pass things from top components to bottom components
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people,setPeople] = useState(data);
+
+  const removePerson = (id) => {
+    setPeople((people)=>{
+      return people.filter((person)=>person.id !== id);
+    })
+  }
+
+  return (
+    <>
+      <h3>propr drilling</h3>
+      <List people={people}/>
+    </>
+  );
 };
+
+const List = ({people})=>{
+  return (
+  <>
+    {
+     people.map((person)=>{
+      return <SinglePerson key={person.id}/>
+     }) 
+    }
+  </>
+  )
+}
+
+const SinglePerson = ({id,item}) => {
+  return (
+  <div>
+    <h4>single item</h4>
+  </div>
+  )
+}
 
 export default PropDrilling;
