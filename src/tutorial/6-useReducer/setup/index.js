37ba50812e80206007ea4c543fff8893948ac41d
Ionut Suciu <suciuionut888@gmail.com>
2024-01-29 15:22:23 +0100

    dispatch example, manage modal states

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -3,32 +3,68 @@ import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
 
+const reducer = (state,action) => {
+  const newItems = [...state.people,action.payload]
+  if(action.type=="ADD_ITEM"){
+    //returning new state
+    return ({...state,people:newItems,isModalOpen:true,modalContent:"item added"});
+  }
+  if(action.type=="NO_VALUE"){
+    return ({...state,isModalOpen:true,modalContent:"please add val"})
+  }
+  if(action.type=="CLOSE_MODAL"){
+    return ({...state,isModalOpen:false})
+  }
+  if(action.type=="REMOVE_ITEM"){
+    if (action.type === 'REMOVE_ITEM') {
+      const newPeople = state.people.filter(
+        (person) => person.id !== action.payload
+      );
+      return { ...state, people: newPeople };
+    }
+  }
+
+  throw new Error("no matching action type")
+} 
+
+const defaultState = {
+  people:[],
+  isModalOpen:false,
+  modalContent:"hello world"
+}
+
 const Index = () => {
   const [name,setName] = useState("");
-  const [people,setPeople] = useState(data);
-  const [showModal,setShowModal] = useState(false);
+  //reducer function gets called when you call dispatch and changes the state.
+  const [state,dispatch] = useReducer(reducer, defaultState)
 
   const handleSubmit = (e) =>{
     e.preventDefault();
+    const newItem = {name}
     if(name){
-      setShowModal(false);
-      setPeople([...people,name]);
+      //always you have to pass proprety "type"
+      dispatch({type:"ADD_ITEM", payload:newItem})
       setName("");
     }else{
-      setShowModal(true);
+      dispatch({type: "NO_VALUE"})
     }
+  }
+
+  const closeModal = () =>{
+    dispatch({type:"CLOSE_MODAL"})
   }
 
   return(
     <>
-    {showModal && <Modal/>}
+    {state.isModalOpen && <Modal closeModal={closeModal} modalContent={state.modalContent}/>}
     <form onSubmit={handleSubmit}>
       <div>
         <input type='text' value={name} onChange={(e)=>setName(e.target.value)}/>
       </div>
       <button type='submit'>add</button>
+      <button onClick={dispatch({type:"REMOVE_ITEM", payload:person.id})}>remove</button>
     </form>
-    {people.map((person)=>{
+    {state.people.map((person)=>{
       return <div>{person.name}</div>
     })}
     </>
