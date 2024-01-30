939430bc58612a4b7337473ce9fe7450906645f4
Ionut Suciu <suciuionut888@gmail.com>
2024-01-30 09:10:07 +0100

    custom hooks easy example to reuse a function, like useFetch with a different url every time, see the final folder

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -5,6 +5,7 @@ import { useFetch } from './2-useFetch'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
 
+//custom hooks example just to see how to make them or how to use other ppls hooks. See the final folder to understand.
 const Example = () => {
   const [loading, setLoading] = useState(true)
   const [products, setProducts] = useState([])
