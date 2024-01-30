ed4eba2070cd02a4dacfea4dde2d41b2c884016e
Ionut Suciu <suciuionut888@gmail.com>
2024-01-30 09:20:11 +0100

    setup example, the last one doesnt have an img so when you iterate you will get: Cannot read properties of undefined

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({image,name,price}) => {
+  return <article className='product'>
+    <h4>{name}</h4>
+    {//last API obj doesnt have a price, if you work with APIs expect this to happen. Youll get an error if you dont plan
+    }
+    <img src={image.url}/>
+    <p>{price}</p>
+  </article>;
 };
 
 export default Product;
